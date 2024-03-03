bb0a97eda000881881cc5523140a8f29d96a8686
bluthej <joffrey.bluthe@e.email>
2024-03-03 13:04:38 +0100

    Re-implement DCEL with indices instead of pointers

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,16 @@
-use std::ptr;
-use std::{fmt::Display, marker::PhantomData, ptr::NonNull};
+use std::fmt::Display;
 
 #[derive(Debug)]
-struct Face {
-    boundary: Link,
-    len: usize,
-}
-
-type Link = Option<NonNull<Edge>>;
-
-#[derive(Debug)]
-struct Edge {
-    next: Link,
-    prev: Link,
-    origin: Vertex,
-}
-
-impl Display for Edge {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let [xo, yo] = self.origin.coords;
-        if let Some(next) = self.next {
-            let [xn, yn] = unsafe { (*next.as_ptr()).origin.coords };
-            write!(f, "({}, {})->({}, {})", xo, yo, xn, yn)
-        } else {
-            write!(f, "({}, {})->(None)", xo, yo)
-        }
-    }
+struct Dcel {
+    vertices: Vec<Vertex>,
+    faces: Vec<Face>,
+    hedges: Vec<Hedge>,
 }
 
 #[derive(Debug)]
 struct Vertex {
     coords: [f32; 2],
-    edge: Link,
-}
-
-impl Vertex {
-    fn new_with_coords(x: f32, y: f32) -> Self {
-        Self {
-            coords: [x, y],
-            edge: None,
-        }
-    }
+    hedge: usize,
 }
 
 impl Display for Vertex {
@@ -50,149 +20,141 @@ impl Display for Vertex {
     }
 }
 
-struct VertexIter<'a> {
-    current: Link,
-    len: usize,
-    _boo: PhantomData<&'a Edge>,
+#[derive(Debug)]
+struct Face {
+    start: usize,
 }
 
-struct EdgeIter<'a> {
-    current: Link,
-    len: usize,
-    _boo: PhantomData<&'a Edge>,
+#[derive(Debug)]
+struct Hedge {
+    origin: usize,
+    twin: usize,
+    face: usize,
+    next: usize,
+    prev: usize,
 }
 
-impl Face {
-    fn new() -> Self {
+struct FaceVerticesIterator<'a> {
+    dcel: &'a Dcel,
+    face: usize,
+    current: usize,
+    done: bool,
+}
+
+struct FaceHedgesIterator<'a> {
+    dcel: &'a Dcel,
+    face: usize,
+    current: usize,
+    done: bool,
+}
+
+impl Dcel {
+    fn iter_face_vertices(&self, face: usize) -> FaceVerticesIterator {
+        FaceVerticesIterator {
+            dcel: self,
+            face,
+            current: self.faces[face].start,
+            done: false,
+        }
+    }
+
+    fn iter_face_hedges(&self, face: usize) -> FaceHedgesIterator {
+        FaceHedgesIterator {
+            dcel: self,
+            face,
+            current: self.faces[face].start,
+            done: false,
+        }
+    }
+
+    fn print_face_hedges(&self, face: usize) {
+        for hedge in self.iter_face_hedges(face) {
+            let origin = hedge.origin;
+            let dest = self.hedges[hedge.next].origin;
+            let [xo, yo] = self.vertices[origin].coords;
+            let [xd, yd] = self.vertices[dest].coords;
+            println!("({}, {}) -> ({}, {})", xo, yo, xd, yd);
+        }
+    }
+
+    fn from_polygon_soup(vertices: &[[f32; 2]], polygons: &[[usize; 3]]) -> Self {
+        let mut verts = Vec::with_capacity(vertices.len());
+        for &coords in vertices {
+            verts.push(Vertex {
+                coords,
+                hedge: usize::MAX,
+            });
+        }
+
+        let mut faces = Vec::with_capacity(polygons.len());
+        let mut hedges = Vec::new();
+        let mut current_hedge_id = 0;
+        for (face, polygon) in polygons.iter().enumerate() {
+            faces.push(Face {
+                start: current_hedge_id,
+            });
+
+            let n_verts = polygon.len();
+            for (iloc, &vert) in polygon.iter().enumerate() {
+                let (prev, next) = match iloc {
+                    0 => (current_hedge_id + n_verts - 1, current_hedge_id + 1),
+                    ivert if ivert == n_verts - 1 => {
+                        (current_hedge_id - 1, current_hedge_id + 1 - n_verts)
+                    }
+                    _ => (current_hedge_id - 1, current_hedge_id + 1),
+                };
+                hedges.push(Hedge {
+                    origin: vert,
+                    twin: usize::MAX,
+                    face,
+                    next,
+                    prev,
+                });
+                verts[vert].hedge = current_hedge_id;
+                current_hedge_id += 1;
+            }
+        }
+
         Self {
-            boundary: None,
-            len: 0,
-        }
-    }
-
-    fn push(&mut self, vert: Vertex) {
-        // SAFETY: it's a linked-list, what do you want?
-        unsafe {
-            let new = NonNull::new_unchecked(Box::into_raw(Box::new(Edge {
-                next: None,
-                prev: None,
-                origin: vert,
-            })));
-            if let Some(old) = self.boundary {
-                // Put the new front before the old one
-                (*old.as_ptr()).next = Some(new);
-                (*new.as_ptr()).prev = Some(old);
-            } else {
-                // If there's no front, then we're the empty list and need
-                // to set the back too.
-                self.boundary = Some(new);
-            }
-            // These things always happen!
-            self.boundary = Some(new);
-            self.len += 1;
-        }
-    }
-
-    fn close(&mut self) {
-        unsafe {
-            let last = self.boundary.unwrap();
-            let mut boundary = last;
-            while let Some(prev) = (*boundary.as_ptr()).prev {
-                boundary = prev;
-            }
-            (*boundary.as_ptr()).prev = Some(last);
-            (*last.as_ptr()).next = Some(boundary);
-            self.boundary = Some(boundary);
-        }
-    }
-
-    pub fn vertex_iter(&self) -> VertexIter {
-        VertexIter {
-            current: self.boundary,
-            len: self.len,
-            _boo: PhantomData,
-        }
-    }
-
-    pub fn edge_iter(&self) -> EdgeIter {
-        EdgeIter {
-            current: self.boundary,
-            len: self.len,
-            _boo: PhantomData,
+            vertices: verts,
+            faces,
+            hedges,
         }
     }
 }
 
-impl Drop for Face {
-    fn drop(&mut self) {
-        unsafe {
-            if let Some(first) = self.boundary {
-                if let Some(last) = (*first.as_ptr()).prev {
-                    (*last.as_ptr()).next = None;
-                    (*first.as_ptr()).prev = None;
-                }
-                let mut current = first;
-                while let Some(next) = (*current.as_ptr()).next {
-                    (*current.as_ptr()).next = None;
-                    (*next.as_ptr()).prev = None;
-                    current = next;
-                }
-            };
-        }
-    }
-}
-
-impl Default for Face {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl<'a> Iterator for VertexIter<'a> {
+impl<'a> Iterator for FaceVerticesIterator<'a> {
     type Item = &'a Vertex;
 
     fn next(&mut self) -> Option<Self::Item> {
-        // While self.front == self.back is a tempting condition to check here,
-        // it won't do the right for yielding the last element! That sort of
-        // thing only works for arrays because of "one-past-the-end" pointers.
-        if self.len > 0 {
-            // We could unwrap front, but this is safer and easier
-            self.current.map(|edge| unsafe {
-                self.len -= 1;
-                self.current = (*edge.as_ptr()).next;
-                &(*edge.as_ptr()).origin
-            })
-        } else {
-            None
+        if self.done {
+            return None;
         }
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.len, Some(self.len))
+        let hedge = &self.dcel.hedges[self.current];
+        let vertex_id = hedge.origin;
+        self.current = hedge.next;
+        let start = self.dcel.faces[self.face].start;
+        if self.current == start {
+            self.done = true;
+        }
+        Some(&self.dcel.vertices[vertex_id])
     }
 }
 
-impl<'a> Iterator for EdgeIter<'a> {
-    type Item = &'a Edge;
+impl<'a> Iterator for FaceHedgesIterator<'a> {
+    type Item = &'a Hedge;
 
     fn next(&mut self) -> Option<Self::Item> {
-        // While self.front == self.back is a tempting condition to check here,
-        // it won't do the right for yielding the last element! That sort of
-        // thing only works for arrays because of "one-past-the-end" pointers.
-        if self.len > 0 {
-            // We could unwrap front, but this is safer and easier
-            self.current.map(|edge| unsafe {
-                self.len -= 1;
-                self.current = (*edge.as_ptr()).next;
-                &(*edge.as_ptr())
-            })
-        } else {
-            None
+        if self.done {
+            return None;
         }
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.len, Some(self.len))
+        let hedge = &self.dcel.hedges[self.current];
+        self.current = hedge.next;
+        let start = self.dcel.faces[self.face].start;
+        if self.current == start {
+            self.done = true;
+        }
+        Some(hedge)
     }
 }
 
@@ -201,21 +163,52 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_face() {
-        let mut face = Face::new();
-        face.push(Vertex::new_with_coords(0., 0.));
-        face.push(Vertex::new_with_coords(1., 0.));
-        face.push(Vertex::new_with_coords(0., 1.));
-        face.close();
+    fn create_one_triangle_dcel_from_polygon_soup() {
+        let vertices = vec![[0., 0.], [1., 0.], [0., 1.]];
+        let polygons = vec![[0, 1, 2]];
 
-        assert_eq!(face.len, 3);
+        let dcel = Dcel::from_polygon_soup(&vertices, &polygons);
 
-        for vertex in face.vertex_iter() {
-            println!("{}", vertex);
+        let verts: Vec<_> = dcel.iter_face_vertices(0).map(|vert| vert.coords).collect();
+        assert_eq!(verts, vertices);
+        let hedges: Vec<_> = dcel.iter_face_hedges(0).map(|hedge| hedge.origin).collect();
+        assert_eq!(hedges, &polygons[0]);
+    }
+
+    #[test]
+    fn create_two_triangle_dcel_from_polygon_soup() {
+        let vertices = vec![[0., 0.], [1., 0.], [1., 1.], [0., 1.]];
+        let polygons = vec![[0, 1, 3], [1, 2, 3]];
+
+        let dcel = Dcel::from_polygon_soup(&vertices, &polygons);
+
+        // Check vertices
+        let verts: Vec<_> = dcel.iter_face_vertices(0).map(|vert| vert.coords).collect();
+        assert_eq!(verts, vec![[0., 0.], [1., 0.], [0., 1.]]);
+        let verts: Vec<_> = dcel.iter_face_vertices(1).map(|vert| vert.coords).collect();
+        assert_eq!(verts, vec![[1., 0.], [1., 1.], [0., 1.]]);
+
+        // Check half-edges
+        let hedges: Vec<_> = dcel.iter_face_hedges(0).map(|hedge| hedge.origin).collect();
+        assert_eq!(hedges, vec![0, 1, 3]);
+        let hedges: Vec<_> = dcel.iter_face_hedges(1).map(|hedge| hedge.origin).collect();
+        assert_eq!(hedges, vec![1, 2, 3]);
+
+        // Check face ids
+        for face in [0, 1] {
+            for hedge in dcel.iter_face_hedges(face) {
+                assert_eq!(hedge.face, face);
+            }
         }
 
-        for edge in face.edge_iter() {
-            println!("{}", edge);
-        }
+        // Check next and prev
+        let nexts: Vec<_> = dcel.iter_face_hedges(0).map(|hedge| hedge.next).collect();
+        assert_eq!(nexts, vec![1, 2, 0]);
+        let nexts: Vec<_> = dcel.iter_face_hedges(1).map(|hedge| hedge.next).collect();
+        assert_eq!(nexts, vec![4, 5, 3]);
+        let prevs: Vec<_> = dcel.iter_face_hedges(0).map(|hedge| hedge.prev).collect();
+        assert_eq!(prevs, vec![2, 0, 1]);
+        let prevs: Vec<_> = dcel.iter_face_hedges(1).map(|hedge| hedge.prev).collect();
+        assert_eq!(prevs, vec![5, 3, 4]);
     }
 }
