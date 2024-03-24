65fcda271626b6300e91f8a09ae7ac8f3875386c
bluthej <joffrey.bluthe@e.email>
2024-03-24 10:10:21 +0100

    Add iterator on graph nodes

--- src/graph.rs
+++ src/graph.rs
@@ -1,3 +1,5 @@
+use std::slice::Iter;
+
 #[derive(Debug, Default)]
 struct Graph<T> {
     arena: Vec<Node<T>>,
@@ -24,6 +26,10 @@ impl<T> Graph<T> {
         self.arena.push(Node::new(data));
         idx
     }
+
+    fn iter(&self) -> Iter<'_, Node<T>> {
+        self.arena.iter()
+    }
 }
 
 impl<T> Node<T> {
@@ -49,7 +55,7 @@ mod tests {
 
     #[test]
     fn create_node() {
-        let node = Node::<usize>::new(0);
+        let node = Node::new(0);
 
         assert_eq!(node.parents.len(), 0);
         assert_eq!(node.children.len(), 0);
@@ -57,7 +63,7 @@ mod tests {
 
     #[test]
     fn add_node_to_graph() {
-        let mut graph = Graph::<usize>::new();
+        let mut graph = Graph::new();
 
         let idx = graph.add(42);
         assert_eq!(idx, 0);
@@ -66,5 +72,16 @@ mod tests {
         let idx = graph.add(314);
         assert_eq!(idx, 1);
         assert_eq!(graph.count(), 2);
+    }
+
+    #[test]
+    fn graph_iter() {
+        let mut graph = Graph::<usize>::new();
+        graph.add(42);
+        graph.add(314);
+
+        let values: Vec<usize> = graph.iter().map(|node| node.data).collect();
+
+        assert_eq!(&values, &[42, 314]);
     }
 }
