6887920c7cced64dca0f32a39061421ecdfed7ab
bluthej <joffrey.bluthe@e.email>
2024-03-24 10:05:08 +0100

    Add nodes to graph

--- src/graph.rs
+++ src/graph.rs
@@ -18,6 +18,12 @@ impl<T> Graph<T> {
     fn count(&self) -> usize {
         self.arena.len()
     }
+
+    fn add(&mut self, data: T) -> usize {
+        let idx = self.arena.len();
+        self.arena.push(Node::new(data));
+        idx
+    }
 }
 
 impl<T> Node<T> {
@@ -47,5 +53,18 @@ mod tests {
 
         assert_eq!(node.parents.len(), 0);
         assert_eq!(node.children.len(), 0);
+    }
+
+    #[test]
+    fn add_node_to_graph() {
+        let mut graph = Graph::<usize>::new();
+
+        let idx = graph.add(42);
+        assert_eq!(idx, 0);
+        assert_eq!(graph.count(), 1);
+
+        let idx = graph.add(314);
+        assert_eq!(idx, 1);
+        assert_eq!(graph.count(), 2);
     }
 }
