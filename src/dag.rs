08bed469f529a13f9c80576aa5cc1486816a3fa7
bluthej <joffrey.bluthe@e.email>
2024-05-04 23:26:27 +0200

    Add documentation

--- src/dag.rs
+++ src/dag.rs
@@ -1,11 +1,29 @@
 use anyhow::{anyhow, Result};
 use std::slice::Iter;
 
+/// A Directed Acyclic Graph (DAG).
+///
+/// It turns out this is the kind of graph you need to represent a trapezoidal map. A tree won't cut
+/// it because some nodes need to have multiple parents. This happens notably when a new edge crosses
+/// multiple trapezoids, in which case these trapezoids are replaces with Y nodes that may share a
+/// common child trapezoid node.
+///
+/// These kinds of graphs are generally implemented using pointers, but this is not very practical
+/// or idiomatic in Rust, so here it is implemented using an arena. This means that it is actually
+/// backed by a simple `Vec`, and we keep track of the nodes using `usize`s. This is not the best
+/// approach when you need to be able to efficiently remove nodes from the arena, but in the present
+/// case we never actually remove nodes.
+///
+/// Note: This implementation is influenced by:
+/// - [This blog post](https://dev.to/deciduously/no-more-tears-no-more-knots-arena-allocated-trees-in-rust-44k6)
+/// - [indextree](https://crates.io/crates/indextree) (I was using it before I realized I needed a
+/// graph instead of a tree)
 #[derive(Debug, Default)]
 pub(crate) struct Dag<T> {
     arena: Vec<Node<T>>,
 }
 
+/// A node of the DAG.
 #[derive(Debug, Default)]
 pub(crate) struct Node<T> {
     pub(crate) data: T,
@@ -14,36 +32,44 @@ pub(crate) struct Node<T> {
 }
 
 impl<T> Dag<T> {
+    /// Constructs a new empty DAG.
     pub(crate) fn new() -> Self {
         Dag { arena: Vec::new() }
     }
 
+    /// Returns the number of nodes in the DAG.
     fn count(&self) -> usize {
         self.arena.len()
     }
 
+    /// Add a new node to the DAG. Returns the index of the node.
     pub(crate) fn add(&mut self, data: T) -> usize {
         let idx = self.arena.len();
         self.arena.push(Node::new(data));
         idx
     }
 
+    /// Get a shared reference to the node with index `idx`, if it exists.
     pub(crate) fn get(&self, idx: usize) -> Option<&Node<T>> {
         self.arena.get(idx)
     }
 
+    /// Get an exclusive reference to the node with index `idx`, if it exists.
     fn get_mut(&mut self, idx: usize) -> Option<&mut Node<T>> {
         self.arena.get_mut(idx)
     }
 
+    /// An iterator over the DAG's nodes.
     pub(crate) fn iter(&self) -> Iter<'_, Node<T>> {
         self.arena.iter()
     }
 
+    /// Append a new node to an existing one.
     pub(crate) fn append_to(&mut self, data: T, idx: usize) -> Result<usize> {
         self.append_to_many(data, &[idx])
     }
 
+    /// Append a new node to several existing ones.
     fn append_to_many(&mut self, data: T, idxs: &[usize]) -> Result<usize> {
         let new_idx = self.add(data);
         for &idx in idxs {
@@ -57,6 +83,7 @@ impl<T> Dag<T> {
         Ok(new_idx)
     }
 
+    /// Insert a new node before an existing one.
     pub(crate) fn insert_before(&mut self, data: T, idx: usize) -> Result<usize> {
         let new_idx = self.add(data);
         // Store old node's parents
