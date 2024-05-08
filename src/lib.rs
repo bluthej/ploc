9244160bfc6e6f70a9ffc630dcfde00691f4dbd1
bluthej <joffrey.bluthe@e.email>
2024-05-08 21:27:14 +0200

    Add error handling for rectilinear locator
    Unsorted inputs should return an error

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,10 @@ mod dcel;
 mod mesh;
 mod winding_number;
 
+use anyhow::{anyhow, Result};
 use dag::Dag;
 use dcel::{Dcel, FaceId, Hedge, HedgeId};
+use itertools::Itertools;
 use mesh::Mesh;
 
 struct TrapMap {
@@ -177,9 +179,14 @@ struct RectilinearLocator {
 }
 
 impl RectilinearLocator {
-    pub fn new(x: Vec<f64>, y: Vec<f64>) -> Self {
-        // TODO: check that `x` and `y` are ordered
-        Self { x, y }
+    pub fn new(x: Vec<f64>, y: Vec<f64>) -> Result<Self> {
+        for z in [&x, &y] {
+            if z.iter().tuple_windows().any(|(z1, z2)| z1 >= z2) {
+                return Err(anyhow!("The input values should be strictly increasing."));
+            }
+        }
+
+        Ok(Self { x, y })
     }
 
     pub fn locate(&self, points: &[[f64; 2]]) -> Vec<Option<usize>> {
@@ -270,11 +277,21 @@ mod tests {
     }
 
     #[test]
-    fn rectilinear_locator_simple_test() {
+    fn unsorted_input_values_in_rectilinear_locator_returns_error() {
+        // x or y has reversed elements
+        assert!(RectilinearLocator::new(vec![0., 2., 1.], vec![0., 1., 2.]).is_err());
+        assert!(RectilinearLocator::new(vec![0., 1., 1.], vec![0., 2., 1.]).is_err());
+        // x or y has two consecutive equal elements
+        assert!(RectilinearLocator::new(vec![0., 1., 1.], vec![0., 1., 2.]).is_err());
+        assert!(RectilinearLocator::new(vec![0., 1., 2.], vec![0., 1., 1.]).is_err());
+    }
+
+    #[test]
+    fn rectilinear_locator_simple_test() -> Result<()> {
         let x = vec![0., 1., 2.];
         let y = vec![0., 1., 2.];
         let points = vec![[0.5, 0.5], [1.5, 0.5], [0.5, 1.5], [1.5, 1.5], [2.5, 2.5]];
-        let locator = RectilinearLocator::new(x, y);
+        let locator = RectilinearLocator::new(x, y)?;
 
         let locations = locator.locate(&points);
 
@@ -288,10 +305,12 @@ mod tests {
                 assert!(point.is_inside(cell));
             }
         }
+
+        Ok(())
     }
 
     #[test]
-    fn rectilinear_locator_edge_cases() {
+    fn rectilinear_locator_edge_cases() -> Result<()> {
         let x = vec![0., 1., 2.];
         let y = vec![0., 1., 2.];
         let points = vec![
@@ -310,7 +329,7 @@ mod tests {
             [1.5, 1.],
             [1.5, 2.], // Top edge counts as outside
         ];
-        let locator = RectilinearLocator::new(x, y);
+        let locator = RectilinearLocator::new(x, y)?;
 
         let locations = locator.locate(&points);
 
@@ -340,10 +359,12 @@ mod tests {
                 assert!(point.is_inside(cell));
             }
         }
+
+        Ok(())
     }
 
     #[test]
-    fn rectilinear_locator_corner_cases() {
+    fn rectilinear_locator_corner_cases() -> Result<()> {
         let x = vec![0., 1., 2.];
         let y = vec![0., 1., 2.];
         let points = vec![
@@ -357,7 +378,7 @@ mod tests {
             [1., 2.], // Top edge counts as outside
             [2., 2.], // Top/Right edge counts as outside
         ];
-        let locator = RectilinearLocator::new(x, y);
+        let locator = RectilinearLocator::new(x, y)?;
 
         let locations = locator.locate(&points);
 
@@ -384,6 +405,8 @@ mod tests {
                 assert!(point.is_inside(cell));
             }
         }
+
+        Ok(())
     }
 
     prop_compose! {
@@ -394,7 +417,7 @@ mod tests {
     }
 
     #[test]
-    fn rectilinear_locator_proptest() {
+    fn rectilinear_locator_proptest() -> Result<()> {
         let (xmin, xmax) = (0., 10.);
         let (ymin, ymax) = (0., 10.);
         let (nx, ny) = (6, 6); // Use numbers that don't divide the sides evenly on purpose
@@ -404,7 +427,7 @@ mod tests {
         let x = (0..=nx).map(|n| xmin + n as f64 * dx).collect_vec();
         let dy = (ymax - ymin) / ny as f64;
         let y = (0..=ny).map(|n| ymin + n as f64 * dy).collect_vec();
-        let locator = RectilinearLocator::new(x, y);
+        let locator = RectilinearLocator::new(x, y)?;
 
         // Create `Mesh` to check the results using the winding number
         let mesh = Mesh::grid(xmin, xmax, ymin, ymax, nx, ny).unwrap();
@@ -423,5 +446,7 @@ mod tests {
                 assert!(point.is_inside(cell));
             }
         });
+
+        Ok(())
     }
 }
