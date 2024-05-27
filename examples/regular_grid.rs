724bd6b85db9b9a9c6259d0d541618efc5dfc0e3
bluthej <joffrey.bluthe@e.email>
2024-05-27 22:45:14 +0200

    Add example for profiling with flamegraph

---
+++ examples/regular_grid.rs
@@ -0,0 +1,20 @@
+use ploc::{Mesh, PointLocator, TrapMap};
+use rand::prelude::*;
+
+fn main() {
+    let (xmin, xmax) = (0., 10.);
+    let (ymin, ymax) = (0., 10.);
+    let n = 200;
+
+    // Create trapezoidal maps
+    let mesh = Mesh::grid(xmin, xmax, ymin, ymax, n, n).unwrap();
+    let trap_map = TrapMap::from_mesh(mesh).build();
+
+    let mut rng = rand::thread_rng();
+    let query: Vec<_> = (0..420_000)
+        .map(|_| [rng.gen::<f64>() * xmax, rng.gen::<f64>() * ymax])
+        .collect();
+
+    let _res = trap_map.locate_many(&query);
+    // println!("{:?}", res);
+}
