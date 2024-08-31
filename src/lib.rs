7d378be630686a80cdd29500e3088fcdb1a2e225
bluthej <joffrey.bluthe@e.email>
2024-08-31 12:13:50 +0200

    Make rectilinear locator pub for benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,4 @@ mod point_location;
 mod winding_number;
 
 pub use mesh::Mesh;
-pub use point_location::{PointLocator, TrapMap};
+pub use point_location::{PointLocator, RectilinearLocator, TrapMap};
